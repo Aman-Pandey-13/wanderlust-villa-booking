0d103c11b4becc47fc06fe87f4f2e586ff54a6f6
Aman-Pandey-13 <pandeyaman995@gmail.com>
2026-04-05 16:26:39 +0530

    Data initialized

--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config({ path: "../.env" });
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
   .then(() => {
@@ -18,7 +20,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({...obj, owner: "686a48252faf0098303cb9a7"}));
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "686a48252faf0098303cb9a7",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
